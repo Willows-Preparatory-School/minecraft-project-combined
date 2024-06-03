e88c56535e1c9df93af30f6a23aa9a3c992f49f7
sweet-axolotl <146225760+sweet-axolotl@users.noreply.github.com>
2024-06-03 08:38:15 -0700

    Update ItemsModule.java

--- tealsmc.mods/items/ItemsModule.java
+++ tealsmc.mods/items/ItemsModule.java
@@ -1,1 +1,15 @@
+package dev.axolotl.testmod.tealsmc.mods.items;
 
+import dev.axolotl.testmod.tealsmodloader.module.Module;
+
+public class ItemsModule extends Module {
+
+    public static final RockSifter rockSifter = new RockSifter();
+    public static final LightningStick lightningStick = new LightningStick();
+    public static final CrystalGrowingItem crystalGrowing = new CrystalGrowingItem();
+
+    public void onLoad() 
+    {
+      parentMod.itemRegistry.newInstance("lightning_stick", lightningStick, "Lightning Stick");
+    }
+}
